f28ad1e891c2194f07fc72a6f63c5d16ca133772
Scott Miller <scottmillerjr@gmail.com>
2021-06-07 20:12:02 -0400

    init

---
+++ function-app.tf
@@ -0,0 +1,32 @@
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "tfdemosa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "${azurerm_resource_group.rg.name}-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "fa" {
+  name                       = "${azurerm_resource_group.rg.name}-function-app"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+}
